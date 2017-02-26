8a5b91b00b915ee704360ce61e189e94840e2158
= <=>
2017-02-26 12:28:13 -0800

    Fix the problem that nested request fields are undefined.

--- routes/trainingplan.js
+++ routes/trainingplan.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var TrainingPlan = require("../models/TrainingPlan");
+var commonUtils = require('servicecommonutils')
 
 router.get("/:userid", function(req, res, next) {
     TrainingPlan.findOne({ 'userId': req.params.userid }, function (err, plan) {
@@ -63,22 +64,22 @@ router.post("/:userid", function(req, res, next) {
         } else {
             plan.startDate = req.body.startDate;
             plan.firstWeekTime =  {
-                sootheTime: req.body['firstWeekTime[sootheTime]'],
-                firstCriedOut: req.body['firstWeekTime[firstCriedOut]'],
-                secondCriedOut: req.body['firstWeekTime[secondCriedOut]'],
-                followingCriedOut: req.body['firstWeekTime[followingCriedOut]'],
+                sootheTime: commonUtils.nestedReqField(req.body, 'firstWeekTime', 'sootheTime'),
+                firstCriedOut: commonUtils.nestedReqField(req.body, 'firstWeekTime', 'firstCriedOut'),
+                secondCriedOut: commonUtils.nestedReqField(req.body, 'firstWeekTime', 'secondCriedOut'),
+                followingCriedOut: commonUtils.nestedReqField(req.body, 'firstWeekTime', 'followingCriedOut'),
             };
             plan.secondWeekTime = {
-                sootheTime: req.body['secondWeekTime[sootheTime]'],
-                firstCriedOut: req.body['secondWeekTime[firstCriedOut]'],
-                secondCriedOut: req.body['secondWeekTime[secondCriedOut]'],
-                followingCriedOut: req.body['secondWeekTime[followingCriedOut]'],
+                sootheTime: commonUtils.nestedReqField(req.body, 'secondWeekTime', 'sootheTime'),
+                firstCriedOut: commonUtils.nestedReqField(req.body, 'secondWeekTime', 'firstCriedOut'),
+                secondCriedOut: commonUtils.nestedReqField(req.body, 'secondWeekTime', 'secondCriedOut'),
+                followingCriedOut: commonUtils.nestedReqField(req.body, 'secondWeekTime', 'followingCriedOut'),
             };
             plan.followingWeekTime = {
-                sootheTime: req.body['followingWeekTime[sootheTime]'],
-                firstCriedOut: req.body['followingWeekTime[firstCriedOut]'],
-                secondCriedOut: req.body['followingWeekTime[secondCriedOut]'],
-                followingCriedOut: req.body['followingWeekTime[followingCriedOut]'],
+                sootheTime: commonUtils.nestedReqField(req.body, 'followingWeekTime', 'sootheTime'),
+                firstCriedOut: commonUtils.nestedReqField(req.body, 'followingWeekTime', 'firstCriedOut'),
+                secondCriedOut: commonUtils.nestedReqField(req.body, 'followingWeekTime', 'secondCriedOut'),
+                followingCriedOut: commonUtils.nestedReqField(req.body, 'followingWeekTime', 'followingCriedOut'),
             };
 
             plan.save(function (err, plan) {
