7f4153fd69a288e29f24e2bf1695902a915fcf3b
Hui Chen <hchen229@gmail.com>
2017-08-13 02:53:39 -0700

    Update handlers

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var conf = require("./config");
 var middlewares = require('service-middlewares')(conf)
-var metric = require('metricsclient')(conf)
-var utils = require('servicecommonutils')
-var winston = utils.getWinston(conf.get('env'))
 
 //routes
 var sleepRecords = require('./routes/sleeprecords');
@@ -27,50 +24,17 @@ app.use('/', index)
 if (conf.get("env") !== 'test') {
     app.use(middlewares.signature_middleware)
 }
-
 // setup routes
 app.use('/sleeprecs', middlewares.auth_middleware, sleepRecords);
 app.use('/babyinfos', middlewares.auth_middleware, babyInfos);
 app.use('/plan', middlewares.auth_middleware, trainingPlan);
 app.use('/trainrecs', middlewares.auth_middleware, trainingRecords);
 
-//catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('404 Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stack trace
-if (conf.get("env") === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.json(utils.encodeResponseBody(req, {
-            message: err.message,
-            error: err,
-            errorCode: 'INTERNAL_FAILURE'
-        }));
-    });
+app.use(middlewares.error_404_middleware);
+if (conf.get("env") !== 'production') {
+    app.use(middlewares.error_500_middleware_dev);
+} else {
+    app.use(middlewares.error_500_middleware_prod)
 }
-
-// production error handler
-// no stack traces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    metric.errorMetric('SleepService:Error:' + req.method + ':' + req.url, err, function (error, jsonObj) {
-        if (error != null)
-            return res.json(utils.encodeResponseBody(req, {
-                message: 'Failed to add metric. \n' + err.message,
-                errorCode: 'INTERNAL_FAILURE'
-            }));
-        res.json(utils.encodeResponseBody(req, {
-            message: err.message,
-            errorCode: 'INTERNAL_FAILURE'
-        }));
-    })
-});
 
 module.exports = app;
